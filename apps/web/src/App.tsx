15ff34abc866913bbb3bc4a40f6e894f5c2eeb37
Ravi515K <koliravi95@gmail.com>
2025-03-02 19:42:42 +0530

    branch: change branch name master to main in local

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,9 +1,9 @@
-import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SidebarWrapper from "./components/Wrappers/SidebarWrapper";
-import Home from "./Pages/HomePage/HomePage";
-import Activity from "./Pages/HomePage/Components/Activity";
-import Opportunity from "./Pages/HomePage/Components/Opportunity";
-import { AnimatePresence } from "framer-motion";
+// import Home from "./Pages/HomePage/HomePage";
+// import Activity from "./Pages/HomePage/Components/Activity";
+// import Opportunity from "./Pages/HomePage/Components/Opportunity";
+// import { AnimatePresence } from "framer-motion";
 import { lazy } from "react";
 const HomePageRouting = lazy(() => import("./Pages/HomePage/HomeRouting"));
 const FormBuilderPage = lazy(
@@ -24,18 +24,18 @@ function App() {
   );
 }
 
-const AnimatedRoutes = () => {
-  const location = useLocation();
+// const AnimatedRoutes = () => {
+//   const location = useLocation();
 
-  return (
-    <AnimatePresence mode="wait">
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<Home />} />
-        <Route path="/opportunities" element={<Opportunity />} />
-        <Route path="/activity" element={<Activity />} />
-      </Routes>
-    </AnimatePresence>
-  );
-};
+//   return (
+//     <AnimatePresence mode="wait">
+//       <Routes location={location} key={location.pathname}>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/opportunities" element={<Opportunity />} />
+//         <Route path="/activity" element={<Activity />} />
+//       </Routes>
+//     </AnimatePresence>
+//   );
+// };
 
 export default App;
